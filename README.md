d773d60433fe3d60e8d7b818346bf786f5c0ad84
Dmitry <m.mezonuk@gmail.com>
2020-10-27 21:16:28 +0000

    added readme.md file with general adjustments

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-# land-ownership
+# Land ownership CLI
+## Table of contents
+* [General info](#general-info)
+* [Usage Details](#usage-details)
+* [Technologies](#technologies)
+* [Setup](#setup)
 
+## General info
+Utility for generating tree of relations between companies
+with aggregation report of used land ownership
 
-python3 setup.py install
+## Usage details
+has two modes:
+--mode=from_root
+draws the relations from the root level
+
+--mode=expand
+expand the tree from the current company level
+
+## Technologies
+Project is created with:
+Python3 version
+
+## Setup
+run project install: python3 setup.py install
+run initiate db: data_feed
+run help: landtree  --h
--- land_commands/command.py
+++ land_commands/command.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from land_commands.db_storage import *
 import argparse
 import sqlite3
@@ -13,10 +14,11 @@ def get_root(company, db_cursor):
 
 
 # tree printer from the node level and current company level
+# [] list tuple values could be wrapped into type of Company with accessors but for sake of quickness I used the [0][1]
 def print_from_root(node: str, level, current_company, db_cursor):
     children = get_by_parent_company(node, 'company_relations', db_cursor)
     for c in children:
-        company = "| - " + str(c[0]) + "; " + c[1] + "; " + str(count_parcel_items(c[0], 'land_ownership', db_cursor))
+        company = " | - " + str(c[0]) + "; " + c[1] + "; " + str(count_parcel_items(c[0], 'land_ownership', db_cursor))
         mark = "" if not c[0] == current_company else "***"
         print(" " * level + str(company) + "  " + mark)
         print_from_root(c[0], level + 1, current_company, db_cursor)
@@ -49,8 +51,10 @@ def main():
     args = parser.parse_args()
     if args.mode == "from_root":
         company_tree_root = get_root(args.company, db_cursor)
+        root_details = get_item(company_tree_root, 'company_relations', db_cursor)
+        print(str(root_details[0][0]) + "; " + str(root_details[0][1]) + "; "
+              + str(count_parcel_items(root_details[0][0], 'land_ownership', db_cursor)))
         print_from_root(company_tree_root, 0, args.company, db_cursor)
-
     if args.mode == "expand":
         print_from_root(args.company, 0, args.company, db_cursor)
     connection.close()
